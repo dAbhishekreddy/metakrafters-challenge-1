af47d32882bc2f87ca490218aa4f91fac95682e4
Doppe Abhishek Reddy <93373877+dAbhishekreddy@users.noreply.github.com>
2023-11-03 19:01:12 +0530

    Updated Contract

--- contracts/challenge1.sol
+++ contracts/challenge1.sol
@@ -1,57 +1,39 @@
+// In this module it is asked to create a smart contract with require(),assert() and revert() statements
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// here we are defining a smart contract named HospitalManagement.
-contract HospitalManagement {
+contract Challenge {
+    function requireExample(int num) external pure returns (int) {
+        //Uses require to validate the given condition 
+        require(num > 0, "The number must be greater than zero");
+        
+        // It performs the multiplication i.e like it the entered number is multipled by 2 
     
-
- // Define a struct to represent patient information.
-    struct Patient {
-        uint id;          
-        string name;
-        //returns boolean value  indicating if the patient is admitted. 
-        bool admitted;    
+        int result = num * 2;
+        
+        return result;
     }
     
-    // Created a mapping to store patient data with the patient ID as the key.
-    mapping(uint => Patient) public patients;
-
-    //here this is used to  track the total number of patients.
-    uint public patientCount;
-
-    //This  function is used  to admit a patient.
-    function admitPatient(uint _id, string memory _name) public {
-        // require statement: Ensure that the patient is not already admitted.
-        require(!patients[_id].admitted, "Patient is already admitted");
+    function assertExample(uint num) external pure returns (uint) {
+        uint result;
         
-        // assert statement: Ensure that the patient's name is not empty.
-        assert(bytes(_name).length > 0);
+        // Uses assert to ensure a condition that must always hold
+        // Here the assert statement checks the given number is not equal to zero  
+        //If it is equal to zero it will give an exception and revert the transaction.
+        assert(num != 0);
         
-        // Creates a new Patient object and add it to the mapping.
-        patients[_id] = Patient(_id, _name, true);
+        // Performs the divison operation
+        result = 10 / num;
         
-        //It increments the patient count.
-        patientCount++;
+        return result;
     }
     
-    // This function is used  to discharge a patient.
-    function dischargePatient(uint _id) public {
-        // require statement checks that the patient is admitted before discharging.
-        require(patients[_id].admitted, "Patient is not admitted");
-        
-        // Mark the patient as discharged.
-        patients[_id].admitted = false;
-        
-        // Decrement the patient count.
-        patientCount--;
-    }
-    
-    //This function is used to update a patient's name.
-    function updatePatientName(uint _id, string memory _name) public {
-        // revert statementis used to revert the transaction if the provided name is empty.
-        if (bytes(_name).length == 0) {
-            revert("Name cannot be empty");
+    function revertExample(bool condition) external pure {
+        // if the condition is true it will revert the transaction and gives the message the condition is true 
+        if (condition) {
+            revert(" The condition is true");
         }
-        patients[_id].name = _name;
     }
 }
